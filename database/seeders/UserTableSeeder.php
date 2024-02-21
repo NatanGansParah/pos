e7660a14b3b35748f5485e82a60c589827eb5517
KingNatan <natanaelac04@gmail.com>
2024-02-21 16:51:52 +0700

    persiapan

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -16,17 +16,17 @@ class UserTableSeeder extends Seeder
     {
         $users = array(
             [
-                'name' => 'Administrator',
+                'name' => 'Admin',
                 'email' => 'admin@gmail.com',
                 'password' => bcrypt('123'),
-                'foto' => '/img/user.jpg',
+                'foto' => '/img/logo-20240212140822.png',
                 'level' => 1
             ],
             [
                 'name' => 'Kasir 1',
                 'email' => 'kasir1@gmail.com',
                 'password' => bcrypt('123'),
-                'foto' => '/img/user.jpg',
+                'foto' => '/img/logo-20240221091434.jpg',
                 'level' => 2
             ]
         );
